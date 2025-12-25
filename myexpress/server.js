c7ce1a478bd2b614e396a84d0b5edb0924fd551e
EM METH <methlovestudentsandwork@gmail.com>
2025-12-25 22:55:55 +0700

    m

--- myexpress/server.js
+++ myexpress/server.js
@@ -1,10 +1,13 @@
 import dotenv from "dotenv";
 import app from "./scr/app.js";
 
+
+
 dotenv.config();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 10000;
 
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
