6d8965148b433e2911a0beedb70c1e3e271573dc
EM METH <methlovestudentsandwork@gmail.com>
2025-12-27 06:20:30 +0700

    up dd

--- myexpress/scr/config/db.js
+++ myexpress/scr/config/db.js
@@ -5,11 +5,10 @@ dotenv.config();
 const db = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
-  process.env.DB_PASS,
+  process.env.DB_PASSWORD, // <-- password here
   {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    password: process.env.DB_PASSWORD,
     dialect: "mysql",
     logging: false,
     dialectOptions: {
