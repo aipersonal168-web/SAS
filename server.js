8922a250c4a16be875d4b720c1faa0e4ff49ee0b
EM METH <methlovestudentsandwork@gmail.com>
2025-12-27 15:05:28 +0700

    update route

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 import dotenv from "dotenv";
-import express from "express";
+import app from "./scr/app.js";
 
 dotenv.config();
 
-const app = express();
+const PORT = process.env.PORT || 5000;
 
-
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
