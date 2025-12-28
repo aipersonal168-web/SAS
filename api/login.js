ea504a734fd6db2eccee33e131e9081e3e9c910f
EM METH <methlovestudentsandwork@gmail.com>
2025-12-28 21:39:09 +0700

    Create login.js

---
+++ api/login.js
@@ -0,0 +1,10 @@
+export default function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ message: 'Method not allowed' });
+  }
+
+  res.status(200).json({
+    success: true,
+    message: 'Login success'
+  });
+}
