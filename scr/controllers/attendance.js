3ed63920e7122b9f7ff9e618d8e464a058745386
EM METH <methlovestudentsandwork@gmail.com>
2025-12-29 20:59:41 +0700

    Update attendance.js

--- scr/controllers/attendance.js
+++ scr/controllers/attendance.js
@@ -1,23 +1,16 @@
-import Attendance from '../models/attendance.js';
+import Attendance from "../models/attendance.js";
 
 // Get all attendance
 export const getAllAttendance = async (req, res) => {
-    const result = await Attendance.findAll();
-    if (result.success) res.json(result);
-    else res.status(500).json(result);
+  const result = await Attendance.findAll();
+  if (result.success) res.json(result);
+  else res.status(500).json(result);
 };
-
 
 export const storeAttendance = async (req, res) => {
   try {
-    const {
-      year_id,
-      semester_id,
-      shift_id,
-      room_id,
-      week,
-      attendance
-    } = req.body;
+    const { year_id, semester_id, shift_id, room_id, week, attendance } =
+      req.body;
 
     // 1️⃣ Count existing students in this room
     const existingCount = await Attendance.count({
@@ -25,21 +18,13 @@ export const storeAttendance = async (req, res) => {
         yearId: year_id,
         semesterId: semester_id,
         shiftId: shift_id,
-        room_id: room_id
-      }
+        room_id: room_id,
+      },
     });
 
-    const incomingCount = attendance.length;
-    console.log(incomingCount);
-    // 2️⃣ Check limit 30
-    if (existingCount + incomingCount > 30) {
-      return res.status(400).json({
-        message: `Room already has ${existingCount} students. You can add only ${30 - existingCount} more.`
-      });
-    }
 
     // 3️⃣ Prepare records
-    const records = attendance.map(item => ({
+    const records = attendance.map((item) => ({
       student_id: item.student_id,
       student_name: item.student_name,
       teacher_name: item.teacher_name,
@@ -51,7 +36,7 @@ export const storeAttendance = async (req, res) => {
       semesterId: semester_id,
       shiftId: shift_id,
       room_id: room_id,
-      week: week
+      week: week,
     }));
 
     // 4️⃣ Insert
@@ -59,9 +44,8 @@ export const storeAttendance = async (req, res) => {
 
     res.json({
       message: "Attendance saved successfully",
-      totalInserted: records.length
+      totalInserted: records.length,
     });
-
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
